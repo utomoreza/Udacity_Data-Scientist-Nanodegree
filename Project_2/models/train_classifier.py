049db843f2520aa84d17314279db4478ead3140a
utomoreza <utomorezadwi@gmail.com>
2021-09-28 10:08:30 +0700

    add documentations

--- Project_2/models/train_classifier.py
+++ Project_2/models/train_classifier.py
@@ -22,21 +22,20 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.externals import joblib
 
-from matplotlib import pyplot as plt
-import seaborn as sns
-
 def load_data(database_filepath):
     """used to load data from DB and return it as
     ready to train/test dataset"""
 
+    # get data from DB
     engine = create_engine(f'sqlite:///{database_filepath}')
     df = pd.read_sql_table('message_category', con=engine)
 
-    engine.dispose()
+    engine.dispose() # turn off DB engine
 
-#     df.drop(columns=['child_alone'], inplace=True)
+    # get all 36 categories
     category_names = list(df.columns[3:])
 
+    # collect X and Y variables for training/testing purpose
     X = df.message.values
     Y = df.iloc[:, 3:].values
 
@@ -49,11 +48,19 @@ regex_url = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-
 eng_stopwords = stopwords.words('english')
 
 def tokenize(text, lemmatizer=lemmatizer, stopwords=eng_stopwords):
-    """used to preprocess text so that it's ready for training"""
+    """used to preprocess text so that it's ready for training
+
+    args:
+    - lemmatizer: WordNetLemmatizer() instance
+    - stopwords: english stop words
+
+    returns:
+    clean_tokens: clean tokens in a list type
+    """
 
     # convert any accented char
     text = unidecode(text)
-    
+
     # convert any URL
     detected_urls = re.findall(regex_url, text)
     for url in detected_urls:
@@ -62,9 +69,10 @@ def tokenize(text, lemmatizer=lemmatizer, stopwords=eng_stopwords):
     # remove any punct
     text = re.sub(regex_punct, ' ', text)
     text = re.sub(r"\s+", " ", text)
-    
+
+    # tokenize using NLTK
     tokens = word_tokenize(text)
-    
+
     # uncase & lemmatize each token
     clean_tokens = []
     for tok in tokens:
@@ -76,7 +84,7 @@ def tokenize(text, lemmatizer=lemmatizer, stopwords=eng_stopwords):
             if clean_tok == uncased_tok:
                 clean_tok = lemmatizer.lemmatize(clean_tok, pos='v')
             clean_tokens.append(clean_tok)
-            
+
     return clean_tokens
 
 def build_model():
@@ -91,15 +99,17 @@ def build_model():
         ('count', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('classifier', MultiOutputClassifier(random_forest))
-#         ('classifier', MultiOutputClassifier(xgboost))
     ])
 
     # set CV parameters
+    # BEAR IN MIND: the more params you specify,
+    # the more computations you need to train the model
     parameters = {
 #         'count__ngram_range': ((1, 1), (1, 2)),
 #         'count__lowercase': (True, False),
 #         'count__max_df': (0.5, 1.0, 2.0),
 #         'count__max_features': (None, 5000, 10000),
+#         'classifier': (random_forest, xgboost),
         'tfidf__use_idf': (True, False)
 #         'tfidf__smooth_idf': (True, False)
     }
@@ -111,15 +121,30 @@ def build_model():
 
 def evaluate_model(model, X_test, Y_test, category_names,):
     """used to evaluate given model by using
-    confusion matrix and classification report"""
+    confusion matrix and classification report
 
+    args:
+    - model (sklearn model)
+    - X_test
+    - Y_test
+    - category_names: list of 36 category names
+
+    returns:
+    None
+    """
+
+    # predict
     y_pred = model.predict(X_test)
 
+    # process confusion matrix and classification report
+    # for each category
     for j in range(y_pred.shape[1]):
         print('TARGET: ', category_names[j])
         print('\t\t---- CONFUSION MATRIX----')
 
-        classes = np.unique(Y_test[:, j]) #sorted(df[category_names[j]].unique())
+        # get labels for current category
+        classes = np.unique(Y_test[:, j])
+
         print(confusion_matrix(Y_test[:, j],
                                y_pred[:, j],
                                labels=classes))
@@ -136,18 +161,20 @@ def save_model(model, model_filepath):
     joblib.dump(model, model_filepath)
 
 def main():
+    # catch args inputted from CLI
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
+
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
